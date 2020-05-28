9b97717cc6970c845012d8cde3592aa64a88fd82
Berkley Brown <berkleydeane@gmail.com>
2020-05-28 19:48:01 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
    new_array = []
   
-    array.each do |num| new_array << num ** 2}
-  end
+    array.each do |num| new_array << num ** 2
+    
+     end
     new_array
-  end  
+   end  
   
